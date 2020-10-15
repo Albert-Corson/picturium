323fda2023d0cf11668612f79bdf219182dbd923
Albert Corson <corsonalbert@gmail.com>
2020-10-15 11:00:31 +0200

    [REFACTOR] changes the id of multiple views

--- app/src/main/java/com/example/picturium/MainActivity.kt
+++ app/src/main/java/com/example/picturium/MainActivity.kt
@@ -2,25 +2,38 @@ package com.example.picturium
 
 import android.os.Bundle
 import android.view.View
+import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var filersManager: FilterButtonsManager
+    lateinit var filterBtnManager: FilterButtonsManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val searchBar: SearchView = findViewById(R.id.searchbar)
-        filersManager = FilterButtonsManager(this)
+        filterBtnManager = FilterButtonsManager(this)
 
+        val searchBar: SearchView = findViewById(R.id.svSearchBar)
         searchBar.setOnQueryTextListener(SearchBarQueryListener(searchBar))
     }
 
     public fun profileBtnOnClick(view: View) {
     }
 
-    public fun uploadBtnClick(view: View) {
+    public fun uploadBtnOnClick(view: View) {
+    }
+
+    public fun mainFilterBtnOnClick(button: View) {
+        if (button !is RadioButton)
+            return
+        filterBtnManager.mainFilterButtonOnClick(button)
+    }
+
+    public fun secondaryFilterBtnOnClick(button: View) {
+        if (button !is RadioButton)
+            return
+        filterBtnManager.secondaryFilterButtonOnClick(button)
     }
 }
