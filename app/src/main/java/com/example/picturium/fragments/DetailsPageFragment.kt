cbe3926b66060f9225391042556f76964414036b
Albert Corson <albert.corson@epitech.eu>
2020-10-25 21:28:40 +0100

    [MOD] details page UI rework

--- app/src/main/java/com/example/picturium/fragments/DetailsPageFragment.kt
+++ app/src/main/java/com/example/picturium/fragments/DetailsPageFragment.kt
@@ -1,72 +1,32 @@
 package com.example.picturium.fragments
 
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
 import com.example.picturium.R
+import com.example.picturium.models.Submission
 import kotlinx.android.synthetic.main.fragment_details_page.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class DetailsPageFragment : Fragment(R.layout.fragment_details_page) {
 
-    private val args: DetailsPageFragmentArgs by navArgs()
+    private lateinit var _submission: Submission
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _submission = navArgs<DetailsPageFragmentArgs>().value.submission
 
-        search_btnReturn.setOnClickListener { returnBtnOnClick() }
+        details_ibReturn.setOnClickListener { _returnBtnOnClick() }
 
-        lifecycleScope.launch {
-            val url: String? = args.submission.getCoverImage()?.link
-            withContext(Dispatchers.Main) {
-                Glide.with(this@DetailsPageFragment)
-                    .load(url)
-                    .error(R.drawable.error)
-                    .listener(object : RequestListener<Drawable> {
-                        override fun onLoadFailed(
-                            e: GlideException?,
-                            model: Any?,
-                            target: Target<Drawable>?,
-                            isFirstResource: Boolean
-                        ): Boolean {
-                            detail_progressBar.isVisible = false
-                            return false
-                        }
-
-                        override fun onResourceReady(
-                            resource: Drawable?,
-                            model: Any?,
-                            target: Target<Drawable>?,
-                            dataSource: DataSource?,
-                            isFirstResource: Boolean
-                        ): Boolean {
-                            detail_progressBar.isVisible = false
-                            text_view_title.isVisible = true
-                            text_view_description.isVisible = true
-                            linear_button_action.isVisible = true
-                            return false
-                        }
-
-                    })
-                    .into(image_view)
-            }
-        }
-        text_view_title.text = args.submission.title
+        details_tvTitle.text = _submission.title
+        details_tvDescription.text = _submission.description
+        details_tbDownvote.textOff = (_submission.downVotes ?: 0).toString()
+        details_tbDownvote.textOn = ((_submission.downVotes ?: 0) + 1).toString()
+        details_tbUpvote.textOff = (_submission.upVotes ?: 0).toString()
+        details_tbUpvote.textOn = ((_submission.upVotes ?: 0) + 1).toString()
     }
 
-    private fun returnBtnOnClick() {
-        findNavController().navigateUp()
+    private fun _returnBtnOnClick() {
+        requireActivity().onBackPressed();
     }
 }
